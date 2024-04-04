37c4de55888560214b6fdf66daa7c0982deb6ccc
MohammadAli <hosseinkhosroabadi45@gmail.com>
2024-04-05 01:53:18 +0330

    [update] change database configuration for deploying

--- Reserveto/wsgi.py
+++ Reserveto/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Reservify.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Reserveto.settings')
 
 application = get_wsgi_application()
