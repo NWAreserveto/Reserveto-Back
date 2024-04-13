efcef5dc2de21bf2270435c0b346fff7edaae62f
MohammadAli <hosseinkhosroabadi45@gmail.com>
2024-04-13 16:40:43 +0330

    [update] add CORS_ALLOWED_ORIGINS in settings.py

--- Reserveto/settings.py
+++ Reserveto/settings.py
@@ -43,17 +43,25 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'reserve',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+CORS_ALLOWED_ORIGINS = [
+    'https://reserveto-back.onrender.com',
+    'http://localhost:3000',
 ]
 
 ROOT_URLCONF = 'Reserveto.urls'
